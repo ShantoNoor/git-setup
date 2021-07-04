ee89ad4775a94292b082301dee398ed628ab4282
Shanto Noor <45696566+ShantoNoor@users.noreply.github.com>
2021-07-04 10:52:08 +0600

    Add files via upload

---
+++ git-setup.sh
@@ -0,0 +1,8 @@
+git config --global user.name "Shanto Noor"
+git config --global user.email "shantonoor900@gmail.com"
+git config --global core.autocrlf true
+git config --global core.editor "code --wait"
+git config --global core.editor "'C:\Program Files\Sublime Text\subl.exe' -w"
+ssh-keygen -t ed25519 -C "shantonoor900@gmail.com"
+eval "$(ssh-agent -s)"
+ssh-add /c/Users/Shanto/.ssh/id_ed25519
