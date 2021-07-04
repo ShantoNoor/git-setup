440feb6dfd1d2e7fa31b24094cea06bc68e67646
Shanto Noor <45696566+ShantoNoor@users.noreply.github.com>
2021-07-04 10:55:38 +0600

    Update

--- git-setup.sh
+++ git-setup.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 git config --global user.name "Shanto Noor"
 git config --global user.email "shantonoor900@gmail.com"
 git config --global core.autocrlf true
-git config --global core.editor "code --wait"
+# git config --global core.editor "code --wait"
 git config --global core.editor "'C:\Program Files\Sublime Text\subl.exe' -w"
 ssh-keygen -t ed25519 -C "shantonoor900@gmail.com"
 eval "$(ssh-agent -s)"
