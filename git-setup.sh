07125e872d21a6608b8e808f19387a7caa6ddac4
Shanto Noor <shantonoor900@gmail.com>
2022-06-02 14:35:25 +0600

    Update

--- git-setup.sh
+++ git-setup.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 git config --global user.name "Shanto Noor"
 git config --global user.email "shantonoor900@gmail.com"
-git config --global core.autocrlf true
+
 # git config --global core.editor "code --wait"
-git config --global core.editor "'C:\Program Files\Sublime Text\subl.exe' -w"
+# git config --global core.editor "'C:\Program Files\Sublime Text\subl.exe' -w"
+
 ssh-keygen -t ed25519 -C "shantonoor900@gmail.com"
 eval "$(ssh-agent -s)"
-ssh-add /c/Users/Shanto/.ssh/id_ed25519
+
+# For Windows
+# git config --global core.autocrlf true
+# ssh-add /c/Users/Shanto/.ssh/id_ed25519
+
+# For Unix
+git config --global core.autocrlf input
+ssh-add ~/.ssh/id_ed25519
